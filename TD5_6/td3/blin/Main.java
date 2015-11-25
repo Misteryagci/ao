a4c2a310b73e6ce4c46576d06f00c99c0df330a0
Kaan Yagci <kaanyagci@pc4.home>
2015-11-25 04:23:48 +0100

    Cleaning of repo

--- TD5_6/td3/blin/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-	public static void main (String[] args)
-	{
-		eCommerce e = new eCommerce();
-	}
-}
--- TD5_6/td3/blin/ProductType.java
+++
@@ -1,7 +0,0 @@
-package td3.blin;
-
-public enum ProductType {
-	Book,
-	CD,
-	Game
-}
